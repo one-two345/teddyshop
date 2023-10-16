8ed57ba5560e20150a6561f8b8e94329d93e9e1f
one-two345 <129900924+one-two345@users.noreply.github.com>
2023-10-16 16:47:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ async function main() {
 main();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'public'));
+app.set('views', path.join(__dirname,'views'));
 
 //set global error variables
 app.locals.errors = null;
